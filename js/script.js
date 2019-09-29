19a8595bff35062b82b8e513e475644f0ad9e63a
Ranjith <ranju08134@gmail.com>
2019-09-29 22:53:15 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let appId="c6b09fa5fa4d87e71ebd519fd66c59c5";
 let units='imperial';
-let searchMethod;
+let searchMethod='zip';
 
 
 function searchWeather(searchTerm){
-   fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}')
+   fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result=> {
+     return result.json();
+   }).then(result=>{
+         init(result);
+   })
 }
+function init(resultFromServer){
+  console.log(resultFromServer);
+}
+document.getElementById('searchBtn').addEventListener('click',()=>{
+    let searchTerm=document.getElementById('searchInput').value;
+    if(searchTerm)
+    searchWeather(searchTerm);
+})
